c2aaaff7843800b4514ba5e0f38c54dc77671da9
Viktoria Loiko <viktoria.loiko@ivkhk.ee>
2025-12-11 13:00:31 +0200

    +new function loadProjects

--- script.js
+++ script.js
@@ -16,23 +16,45 @@ const allowedRepos = [
     "PhoneStore"
 ];
 
+async function repoHasPages(repoName) {
+    // URL GitHub Pages
+    const url = `https://${username}.github.io/${repoName}/`;
+
+    try {
+        const res = await fetch(url, { method: "HEAD" });
+
+        // Если статус не 404 → страница существует
+        return res.ok;
+    } catch {
+        return false;
+    }
+}
+
 async function loadProjects() {
     const res = await fetch(`https://api.github.com/users/${username}/repos`);
     const repos = await res.json();
 
     projectGrid.innerHTML = "";
 
-    repos
-        .filter(r => allowedRepos.includes(r.name))
-        .forEach(repo => {
-            projectGrid.innerHTML += `
-                <div class="card">
-                    <h3>${repo.name}</h3>
-                    <p>${repo.description || "Проект без описания"}</p>
-                    <a href="${repo.html_url}" target="_blank">Открыть репозиторий</a>
-                </div>
-            `;
-        });
+    for (const repo of repos.filter(r => allowedRepos.includes(r.name))) {
+        const hasDemo = await repoHasPages(repo.name);
+
+        // Если есть index.html → открыть GitHub Pages
+        const link = hasDemo
+            ? `https://${username}.github.io/${repo.name}/`
+            : repo.html_url;
+
+        const btnText = hasDemo ? "Открыть сайт" : "Открыть репозиторий";
+
+        projectGrid.innerHTML += `
+            <div class="card">
+                <h3>${repo.name}</h3>
+                <p>${repo.description || "Проект без описания"}</p>
+                <a href="${link}" target="_blank">${btnText}</a>
+            </div>
+        `;
+    }
 }
 
 loadProjects();
+
